8541a490f8057989b81894f9f715bc22713f2940
fernando737 <fernando.perezdp1@gmail.com>
2020-04-09 22:13:18 +0000

    Add gitpod files a

---
+++ s3_upload/upload/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+#Create your views here 
+
+app_name="upload"
+
+urlpatterns = [
+    path('',views.Index.as_view(), name="Index"),
+]
---
+++ s3_upload/upload/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+from django.views import View
+from . import views
+
+#Import AWS SDK for python Boto
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+#Create your views here.
+class Index(View):
+    def get(self,request):
+        return render(request,"upload/index.html",{})
+    def post(self, request):
+
