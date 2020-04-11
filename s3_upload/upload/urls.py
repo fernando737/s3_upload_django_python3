dc5886928816549976f9c8efe6e3f6593a602da7
fernando737 <fernando.perezdp1@gmail.com>
2020-04-11 02:49:46 +0000

    Add files for testing the upload

--- s3_upload/upload/urls.py
+++ s3_upload/upload/urls.py
@@ -6,4 +6,6 @@ app_name="upload"
 
 urlpatterns = [
     path('',views.Index.as_view(), name="index"),
+    path('sign_s3', views.SignS3.as_view(), name="sign_s3"),
+    path('upload_result', views.UploadResult.as_view(),name="upload_result"),
 ]
--- s3_upload/upload/views.py
+++ s3_upload/upload/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
 from . import views
+import os
+import json
 
 #Import AWS SDK for python Boto
 import boto3
@@ -9,7 +11,38 @@ from botocore.exceptions import NoCredentialsError
 #Create your views here.
 class Index(View):
     def get(self,request):
-        return render(request,"upload/index.html",{})
-    def post(self, request):
+        AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_KEY=os.environ.get('AWS_SECRET_KEY')
+        S3_BUCKET=os.environ.get('S3_BUCKET')
         return render(request,"upload/index.html",{})
 
+class SignS3(View):
+    def get(self,request):
+        S3_BUCKET = os.environ.get('S3_BUCKET')
+
+        file_name = request.args.get('file_name')
+        file_type = request.args.get('file_type')
+
+        s3 = boto3.client('s3')
+
+        presigned_post = s3.generate_presigned_post(
+            Bucket = S3_BUCKET,
+            Key = file_name,
+            Fields = {"acl": "public-read", "Content-Type": file_type },
+            Conditions = [
+                {"acl": "public-read"},
+                {"Content-Type": file_type},
+            ],
+            ExpiresIn=3600,
+        )
+
+        return json.dumps({
+            'data': presigned_post,
+            'url': 'https//%s.s3.amazonaws.com/%s' % (S3_BUCKET,file_name)
+        })
+
+class UploadResult(View):
+    def post(self,request):
+        description=request.POST['description']
+        image-url=request.POST['image-url']
+        return render(request,"upload/result.html",{"image-url":image-url, "description":description})
